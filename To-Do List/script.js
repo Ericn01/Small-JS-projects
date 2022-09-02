872fde0748c9ae00705cca07c600aeff499feae0
Eric Nielsen <eniel662@mtroyal.ca>
2022-09-02 12:17:19 -0600

    Simplified and fixed the displayItems() function

--- To-Do List/script.js
+++ To-Do List/script.js
@@ -1,21 +1,29 @@
-let toDoItems = [];
+let toDoItemsHistory = [];
+
+// Displays the to-do list items on the screen
 function displayItem(){
     let listItem = document.getElementById('to-do-item').value;
-    if (!contains(listItem)){
-        toDoItems.push(listItem)
-        for (let i = 0; i < toDoItems.length; i++){
-            document.getElementById('list-items').innerHTML += '<ul> ' + toDoItems[i] + '</ul>'; 
-        }
+    if (!contains(listItem) && listItem !== ""){ // we don't want to add an empty list item or an item that already exists in the list
+        toDoItemsHistory.push(listItem)
+        document.getElementById('list-items').innerHTML += "<h3> <input type='checkbox' id='item'> " + listItem;
     };
 }
 
+// Clears the to do list 
 function clear(){
-    document.getElementById('list-items').innerHTML = "";
+    document.getElementById('list-items').innerHTML = '';
+    toDoItemsHistory.forEach(listItem =>
+    {
+        listItem = "";
+    })
 }
-// Checks to see if given element is in the array.
+
+// Checks to see if given element is in the item list array.
 function contains(elem){
-    for (let i = 0; i < toDoItems.length; i++){
-        if (elem === toDoItems[i]){return true};
+    for (let i = 0; i < toDoItemsHistory.length; i++){
+        if (elem === toDoItemsHistory[i]){
+            return true
+        }
     }
     return false;
 }
